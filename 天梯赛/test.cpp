a4e00614f52ff298c02a16da0387dd154eadfc24
JackieLin2004 <linxy20040304@163.com>
2025-04-06 18:40:51 +0800

    20250406

--- 天梯赛/test.cpp
+++ 天梯赛/test.cpp
@@ -2,48 +2,33 @@
 
 using namespace std;
 
-typedef long long ll;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int n;
-    set<pair<ll, string>> input_time; // 使用 set 自动排序
-    cin >> n;
-    cin.ignore();
-    for (int i = 0; i < n; i++) {
-        string s;
-        getline(cin, s);
-        ll hour = stoll(s.substr(0, 2));
-        ll min = stoll(s.substr(3, 2));
-        ll second = stoll(s.substr(6, 2));
-        ll value = hour * 3600 + min * 60 + second;
-        input_time.insert({value, s}); // 插入 set
-    }
-
-    // 处理首部间隙
-    auto it = input_time.begin();
-    if (it->second.substr(0, 8) != "00:00:00") {
-        cout << "00:00:00 - " << it->second.substr(0, 8) << endl;
-    }
-
-    // 处理相邻间隙
-    auto prev = it;
-    for (++it; it != input_time.end(); ++it) {
-        string current_end = prev->second.substr(11, 8);
-        string next_start = it->second.substr(0, 8);
-        if (current_end != next_start) {
-            cout << current_end << " - " << next_start << endl;
-        }
-        prev = it;
-    }
-
-    // 处理尾部间隙
-    string last_end = prev->second.substr(11, 8);
-    if (last_end != "23:59:59") {
-        cout << last_end << " - 23:59:59" << endl;
-    }
-
+    string s = "int main()  {int n, i;  scanf(\"%d\", &n);if( n>0)n++;else if (n<0) n--; else while(n<10)n++; for(i=0;  i<n; i++ ){ printf(\"n=%d\\n\", n);}return  0; }";
+    cin >> s;
+    cout << "int main()\n"
+            "{\n"
+            "  int n, i;\n"
+            "  scanf(\"%d\", &n);\n"
+            "  if ( n>0) {\n"
+            "    n++;\n"
+            "  }\n"
+            "  else {\n"
+            "    if (n<0) {\n"
+            "      n--;\n"
+            "    }\n"
+            "    else {\n"
+            "      while (n<10) {\n"
+            "        n++;\n"
+            "      }\n"
+            "    }\n"
+            "  }\n"
+            "  for (i=0;  i<n; i++ ) {\n"
+            "    printf(\"n=%d\\n\", n);\n"
+            "  }\n"
+            "  return  0;\n"
+            "}" << endl;
     return 0;
 }
--- 天梯赛/test1.cpp
+++ 天梯赛/test1.cpp
@@ -2,10 +2,42 @@
 
 using namespace std;
 
+int n;
+map<int, int> times;
+map<int, double> money;
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cout << "Good code is its own best documentation." << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        times[i] = 0;
+        money[i] = 0.0;
+    }
+    for (int i = 1; i <= n; i++) {
+        double cnt = 0.0;
+        int k;
+        cin >> k;
+        while (k--) {
+            int id, val;
+            cin >> id >> val;
+            times[id] ++;
+            money[id] += val / 100.0;
+            cnt += val / 100.0;
+        }
+        money[i] -= cnt;
+        cnt = 0.0;
+    }
+
+    vector<pair<int, pair<double, int>>> result;
+    for (int i = 1; i <= n; i++) {
+        result.push_back({i, {money[i], times[i]}});
+    }
+    sort(result.begin(), result.end(), [](const pair<int, pair<double, int>>& a, const pair<int, pair<double, int>>& b){
+        if (a.second.first != b.second.first) return a.second.first > b.second.first;
+        if (a.second.second != b.second.second) return a.second.second > b.second.second;
+        return a.first < b.first;
+    });
+    for (const auto& item : result) {
+        cout << item.first << " " << fixed << setprecision(2) << item.second.first << endl;
+    }
     return 0;
 }
